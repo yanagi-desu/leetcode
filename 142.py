2a0ab0f6682279e744e0a1c80c0e255703434470
yanagi-desu <byang28@g.ecc.u-tokyo.ac.jp>
2021-04-04 03:30:16 +0900

    improved Q142

--- 142.py
+++ 142.py
@@ -4,15 +4,37 @@ class ListNode:
         self.val = x
         self.next = None
 
+
+
 class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
-        viewed = []
-        i=0
-        while head:
-            if head in viewed:
-                return head
-            else:
-                i+=1
-                viewed.append(head)
-                head = head.next
+        if not head or not head.next:
+            return None
+        onestep = head
+        twostep = head
+        hasCycle = False
+        while onestep and twostep:
+            onestep = onestep.next
+            if not twostep.next:
+                return None
+            twostep = twostep.next.next
+            if twostep == onestep: 
+                hasCycle = True
+                break 
+        if hasCycle:
+            onestep = head
+            while onestep!=twostep:
+                onestep = onestep.next
+                twostep = twostep.next
+            return onestep
         return None
+
+head = ListNode(3)
+i = [2,0,-4]
+cur = head
+while i:
+    cur.next = ListNode(i.pop(0))
+    cur = cur.next
+
+solve = Solution()
+print(solve.detectCycle(head))
